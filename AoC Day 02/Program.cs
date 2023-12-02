3fbee8dfc84e1e254bf66e78ab3e9405227540f8
bordy82 <kborduas@gmail.com>
2023-12-02 08:32:06 -0500

    AoC Day 02 Solutions 1 and 2

--- AoC Day 02/Program.cs
+++ AoC Day 02/Program.cs
@@ -1,18 +1,52 @@
 ﻿using Utilities;
 
-SolvePuzzleOne();
-SolvePuzzleTwo();
+SolvePuzzleOneAndTwo();
 
-void SolvePuzzleOne()
+void SolvePuzzleOneAndTwo()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
 
-    Console.WriteLine($"Réponse 1 : ");
-}
+    var power = 0;
+    var sumGameIds = 0;
 
-void SolvePuzzleTwo()
-{
-    var data = DataLoader.GetStringDataFromFile(true);
+    foreach (var game in data)
+    {
+        var colors = new Dictionary<string, int>();
+        var gameId = int.Parse(game.Substring(5, game.IndexOf(':') - 5));
 
-    Console.WriteLine($"Réponse 2 : ");
+        foreach (var draw in game.Substring(game.IndexOf(':') + 2).Split("; "))
+        {
+            foreach(var packs in draw.Split(", "))
+            {
+                var count = int.Parse(packs.Split(' ').First());
+                var color = packs.Split(' ').Last();
+
+                if (colors.ContainsKey(color))
+                {
+                    if (colors[color] < count)
+                    {
+                        colors[color] = count;
+                    }
+                }
+                else
+                {
+                    colors.Add(color, count);
+                }
+            }
+        }
+
+        var setPower = 1;
+        foreach (var set in colors)
+        {
+            setPower *= set.Value;
+        }
+
+        power += setPower;
+
+        if (colors["red"] <= 12 && colors["green"] <= 13 && colors["blue"] <= 14)
+            sumGameIds += gameId;
+    }
+
+    Console.WriteLine($"Réponse 1 : " + sumGameIds);
+    Console.WriteLine($"Réponse 2 : " + power);
 }
