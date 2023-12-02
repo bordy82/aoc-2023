4fd95da45d94e2a3d08e4abd4853500907578a65
bordy82 <kborduas@gmail.com>
2023-12-01 20:11:49 -0500

    Prep for Day 2

---
+++ AoC Day 03/Program.cs
@@ -0,0 +1,18 @@
+﻿using Utilities;
+
+SolvePuzzleOne();
+SolvePuzzleTwo();
+
+void SolvePuzzleOne()
+{
+    var data = DataLoader.GetStringDataFromFile(true);
+
+    Console.WriteLine($"Réponse 1 : ");
+}
+
+void SolvePuzzleTwo()
+{
+    var data = DataLoader.GetStringDataFromFile(true);
+
+    Console.WriteLine($"Réponse 2 : ");
+}
